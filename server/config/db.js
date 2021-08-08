051361756e4b45631386d5b2bff088f190130674
wsx2312 <wsx2312@naver.com>
2021-08-08 16:03:47 +0900

    confirm MySQL operation

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,21 @@
 const mysql = require('mysql')
-require('dotenv').config()
+const path = require('path')
+require('dotenv').config({
+    path: path.resolve(__dirname, '../.env')
+})
+
 
 const db_info = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+   /*
+    host: 'localhost',
+    user: 'mvptest',
+    password: '111111',
+    database: 'MVP1'
+    */
 }
 
 const db = mysql.createConnection(db_info)
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/db.js')
 
 router.get('/', (req, res) => {
+    db.query(`SELECT * FROM \`like\``, (err, rows, cols) => {
+        if (err) throw err
+        else {
+            for(let i = 0; i < rows.length; i++) {
+              console.log(rows[i].cookie_id + ": " + rows[i].content_id)
+            }
+        }
+    })
     console.log("항상", req.baseUrl)
-    res.json({message:"서버에서 돌려주는 값"});
+    res.json({message:"서버에서 돌려주는 값"}); // data에 객체로. end와 같음.
 })
+
 
 module.exports = router;
