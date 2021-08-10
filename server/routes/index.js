c8378a54e83226ecfd0e20c3a8c2d5eed2e7943f
wsx2312 <wsx2312@naver.com>
2021-08-10 13:44:03 +0900

    adding the minimum skeletion of functions to understand

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db.js')
+const cookieParser = require('cookie-parser')
+const uuidv4 = require('uuid/v4')
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+router.use(cookieParser());
 
 router.get('/', (req, res) => {
+    /*
     db.query(`SELECT * FROM \`like\``, (err, rows, cols) => {
         if (err) throw err
         else {
@@ -11,8 +18,49 @@ router.get('/', (req, res) => {
             }
         }
     })
-    console.log("항상", req.baseUrl)
-    res.json({message:"서버에서 돌려주는 값"}); // data에 객체로. end와 같음.
+    */
+    //console.log("항상", req.baseUrl)
+    res.cookie('cookie_id', uuidv4(), {maxAge: 2629800}) // universally unique identifier to uniquely identify something.
+    res.json({message:"generating cookie_id"}); // data에 객체로. end와 같음.
+})
+router.get('/getGallery', (req, res) => {
+    db.query(`SELECT * FROM gallery`, (err, rows, cols) => {
+        if (err) throw err
+        else {
+            console.log("getGallery okay.")
+            res.json({items: rows}) // sending rows(array data)
+        }
+    })
+}) // 갤러리 정보 전부 다.
+
+router.post('/uploadGallery', (req, res) => { // VALUES are in req.body except cookie_id
+    db.query(`INSERT INTO gallery(cookie_id, content_title, content_desc, category, writer, \`like\`, content_image) 
+    VALUES(${req.cookies.cookie_id}, ${req.body.content_title}, ${req.body.content_desc}, ${req.body.category}, ${req.body.writer}, ${req.body.like}, ${req.body.content_image})`, (err, rows, cols) => {
+        if (err) throw err
+        else {
+            console.log("uploadGallery okay.")
+        }
+    })
+}) // 포스트로 글 작성.
+
+router.post('/updateLike', (req, res) => { // update되는 게시글의 id가 body로 전달되어야 함.
+    let isEqual = false;
+    db.query(`SELECT * FROM \`like\` WHERE content_id = ${req.body.id}`, (err, rows, cols) => {
+        if (err) throw err
+        else {
+            console.log("updateLike 1 okay.")
+            for (let i = 0; i < rows.length; ++i) {
+                if (rows[i].cookie_id === req.cookies.cookie_id) {
+                    isEqual = true; // DO NOT press like button!
+                }
+            }
+        }
+    })
+    if (!isEqual) {
+        db.query(`UPDATE `) // updating the number of like
+
+        db.query(`INSERT INTO`) // inserting cookie_id into like table
+    }
 })
 
 
